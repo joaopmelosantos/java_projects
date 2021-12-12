6b61fe1837e4fe38575e11e6a6e35830e22fa47b
Joao Paulo <joaopmelosantos@gmail.com>
2021-12-12 12:34:05 -0300

    Christmas Tree Done!

---
+++ ChristmasTree.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ChristmasTree {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int lines;
+
+    System.err.println("Type the number: ");
+    lines = input.nextInt();
+    input.close();
+
+    for (int i = 0; i < lines; i++) {
+      for (int j = 0; j < lines - i; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k <= i; k++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+
+}
